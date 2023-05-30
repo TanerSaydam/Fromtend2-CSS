fba2da1e422a34ee2521fe4d0847638f00fcb03d
tanersaydam <tanersaydam@gmail.com>
2023-05-30 22:19:40 +0300

    .

--- blogs/blog1/index.js
+++ blogs/blog1/index.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".multiple-text", {
-    strings: ["Frontend Developer","Youtuber", "Blogger"],
+    strings: ["Frontend Developer","Youtuber", "Blogger", ".Net Developer"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
